711c48ab103e08f1610a06c38d8eedd6b176d755
Nawin Khamchun <nawin3399@gmail.com>
2022-12-21 11:59:31 +0700

    update code - 21/12/2022

---
+++ Lecture-2-DataType/demovarletconst.js
@@ -0,0 +1,17 @@
+var hello = "Hello"
+console.log(hello)
+hello = "Hello world"
+console.log(hello)
+
+if (true) {
+    let world = "Hello world"
+    console.log(world)
+}
+
+//console.log(world)
+
+const fixval = "Fix vaule"
+console.log(fixval)
+
+
+//fixval = "tet asdasd"
