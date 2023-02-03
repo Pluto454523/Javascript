0bd00b680462cec36f96e555a7381dec10d4cd6f
PLUTo <nawin3399@gmail.com>
2023-02-04 00:30:20 +0700

    update

--- midterm_example/cap_to_front.js
+++ midterm_example/cap_to_front.js
@@ -5,11 +5,11 @@ function capToFront(str) {
 
     let sUpper = "" , sLowwer = ""
     for (const strChar of str)
-        (strChar.toUpperCase() === strChar) ? sUpper += s : sLowwer += s;
+        (strChar.toUpperCase() === strChar) ? sUpper += strChar : sLowwer += strChar;
     return sUpper + sLowwer
 }
 
-console.log( capToFront("hApPy") );
-console.log( capToFront("moveMENT") );
-console.log( capToFront("shOrtCAKE") );
+console.log( capToFront("hApPy") );  //APhpy
+console.log( capToFront("moveMENT") ); //MENTmove
+console.log( capToFront("shOrtCAKE") ); //OCAKEshrt
 
