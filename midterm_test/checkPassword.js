8a6e7ba9ae0e761bb59b41993df328455ecfc8ff
PLUTo <nawin3399#gmail.com>
2023-02-08 09:04:19 +0700

    test

--- midterm_test/checkPassword.js
+++ midterm_test/checkPassword.js
@@ -1,7 +1,7 @@
 function checkPassword(password) {
 
     let result = ""
-    const special = "!@#$%^&*+=._-aa"
+    const special = "!@#$%^&*+=._-"
 
     if ( password.length < 8) 
         result += "Password must be at least 8 characters long.";
